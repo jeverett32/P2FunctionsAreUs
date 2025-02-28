5f94ffaec303b7a1f41003e56fdc92027f37bdfb
mbrown-40 <mbrow238@byu.edu>
2025-02-27 19:43:11 -0700

    Welcome, Explanation, function
    I wrote a function to collect the players name, explain the rules of the game, and then an output of the players name and a call for the function.

--- soccerCode.py
+++ soccerCode.py
@@ -1,6 +1,28 @@
 # This is my original code for assignment A4 
-
 # initialize lists 
+
+#Meagan Brown
+#Creating a function that explains how the game will work
+#Collects the name of the player
+#Returns the name to be used in the program later.
+#Create a function
+def get_player_name():
+    #A welcome introduction to explain how the game will work
+    print("Welcome to the Soccer Team Tournament Matchup!")
+    print("\n   How to Play:")
+    print("   1. Choose your team from the list provided.")
+    print("   2. Select an opponent to challenge.")
+    print("   3. The selected teams will play against each other in an intense match.")
+    print("   4. Check out the final score and see which team comes out on top.\nMay the best team win!")
+    #Collect the player's name
+    name=input("\nTo start, enter your name: ")
+    #Personalized welcome message
+    print(f"Welcome, {name}! Get ready to play!")
+    #Returning name to be used in the program
+    return name
+#Call the function to get the player's name
+player_name= get_player_name()
+
 Soccer = {}
 Season = []
 Games = []
