b401060ce59977e4c7bab91dc3bb21c65e4aed64
John Everett <john.everett32@gmail.com>
2025-03-01 17:25:47 -0700

    Update updatedSoccerCode.py

--- updatedSoccerCode.py
+++ updatedSoccerCode.py
@@ -41,8 +41,8 @@ while iChoice !=4 :
         GameCt = int(GameCt) 
         dictSoccer[HomeTm] = []
         #for Ct in range(GameCt):
-         #   OpTm = OpponentSelection()
-          #  print(f"{HomeTm} is playing against {OpTm}")
+        #   OpTm = OpponentSelection()
+        #  print(f"{HomeTm} is playing against {OpTm}")
         
         dictSoccer, iWins, iLosses = play_game(dictSoccer, HomeTm, iWins, iLosses, GameCt)
         iChoice = menu()
